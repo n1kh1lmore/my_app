72ea687d58230039da5ef2731758aa41b8b45206
n1kh1lmore <nikupcm10@gmail.com>
2022-08-31 15:57:43 +0530

    list random words

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,61 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'my_app',
+      // title: 'Startup Name Generator',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello world'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          appBar: AppBar(
+            // title: const Text('my_app'),
+            title: const Text('Startup Name Generator'),
+          ),
+          body: const Center(
+            child: RandomWords(),
+
+            // body: Center(
+            //   child: Text(wordPair.asPascalCase),
+          )
+          // ),
+          ),
     );
   }
+}
+
+// @override
+// State<RandomWords> createState() => _RandomWordsState();
+// }
+
+class _RandomWordsState extends State<RandomWords> {
+  // return Container();
+  //   final wordPair = WordPair.random();
+  // return Text(wordPair.asPascalCase);
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+  State<RandomWords> createState() => _RandomWordsState();
 }
